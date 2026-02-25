bbc6ea37e055be9d246d1295e9913b62a9fb4b35
Derrick Ng <dng.lacity@gmail.com>
2026-02-24 22:53:59 -0800

    2.24.26 2253

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sweatdex/main.dart';
+import 'package:apexondeck/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
