97d3b4e228d5b91eef857c0e03e0b00adbbb52ce
Derrick Ng <dng.lacity@gmail.com>
2026-02-28 18:23:58 -0800

    2.28.26 1840 web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1740819761" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3009571392" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
